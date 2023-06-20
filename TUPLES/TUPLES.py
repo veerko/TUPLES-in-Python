8e18b1b464fd168fb64723bd5453b41e4b66ebc1
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 07:26:00 +0300

    added some documentation

--- TUPLES/TUPLES.py
+++ TUPLES/TUPLES.py
@@ -3,7 +3,7 @@
 #alist name etc
 alist = ["st1ko", "hackwell", 92]
 
-
+#change the alist name with the index so index 0 is st1ko, but [0] = zenuu so it changes the name 
 alist [0] = "zenuu"
 
 print (alist)
