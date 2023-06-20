4139618b43e480ccf1530690fe48eda5dfa65461
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 07:28:57 +0300

    added documentation

--- TUPLES/TUPLES.py
+++ TUPLES/TUPLES.py
@@ -6,8 +6,12 @@ alist = ["st1ko", "hackwell", 92]
 #change the alist name with the index so index 0 is st1ko, but [0] = zenuu so it changes the name 
 alist [0] = "zenuu"
 
-print (alist)
+#print (alist)
 
 atuple = ["st1ko", "hackwell", 92]
+
+atuple [0] = "zenuu"
+
+print (atuple)
 
 
