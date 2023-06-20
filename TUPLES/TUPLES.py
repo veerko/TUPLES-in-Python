dde44290a15b905a37099298a68d1956fabd261b
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 07:36:14 +0300

    added documentation

--- TUPLES/TUPLES.py
+++ TUPLES/TUPLES.py
@@ -6,12 +6,28 @@ alist = ["st1ko", "hackwell", 92]
 #change the alist name with the index so index 0 is st1ko, but [0] = zenuu so it changes the name 
 alist [0] = "zenuu"
 
-#print (alist)
+print (alist)
 
 atuple = ["st1ko", "hackwell", 92]
 
 atuple [0] = "zenuu"
 
 print (atuple)
+
+# tuples is much faster, so to prove it we are going to do a test 
+
+#list speed test
+import timeit
+
+print(timeit.timeit(stmt='["red", "blue", "green", 5, 7, 12, 18, "dude"]', nummber=10000000))
+
+
+#tuple speed test
+
+print(timeit.timeit(stmt='("red", "blue", "green", 5, 7, 12, 18, "dude")', nummber=10000000))
+
+
+
+
 
 
